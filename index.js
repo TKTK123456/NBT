4377f76c9dbfe0ee97bbe22a096f209f44a6ba1e
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2024-11-19 01:54:28 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as NBT from "nbtify";
 import fs from "node:fs";
 async function convertFromDat(fromUrl) {
-const file = fs.readFileSync(fromUrl);
+const file = fs.readFileSync("./b+M7ZwFnAgA=/level.dat_old");
 let string = await NBT.read(file)
 string = NBT.stringify(string)
 fs.writeFileSync("./level.txt", string)
@@ -9,7 +9,8 @@ fs.writeFileSync("./level.txt", string)
 async function convertToDat(toUrl) {
 const file = fs.readFileSync("./level.txt");
 let output = NBT.parse(file.toString())
+console.log(output)
 output = await NBT.write(output)
-fs.writeFileSync(toUrl, output)
+fs.writeFileSync("./b+M7ZwFnAgA=/level.dat_old", output)
 }
-convertToDat("./b+M7ZwFnAgA=/level.dat_old")
+convertToDat("./b+M7ZwFnAgA=/level.dat")
